0827782ad63133f2bd7b6cd33e5763b86e77d421
Joe Melia <jmelia@students.nortwheststate.edu>
2025-07-21 22:07:50 -0400

    assignment completed

--- code.py
+++ code.py
@@ -3,9 +3,10 @@ from adafruit_circuitplayground import cp
 import time
 
 class Region:
-    def __init__(self, color, leds):
+    def __init__(self, color, leds, tone):
         self.__color = color
         self.__leds = leds
+        self.__tone = tone
 
     def get_color(self):
         return self.__color
@@ -25,13 +26,18 @@ class Region:
         for off in self.__leds:
             cp.pixels[off] = (0, 0, 0)
 
-yellow = Region((255, 255, 0), (5, 6, 7))
-blue = Region((0, 0, 255), (2, 3, 4))
+    def play_tone(self, duration):
+        cp.play_tone(self.__tone, duration)
+
+regions = {"yellow": Region((255, 255, 0), (5, 6, 7), 252),
+           "blue": Region((0, 0, 255), (2, 3, 4), 209),
+           "red": Region((255, 0, 0), (7, 8, 9), 310),
+           "green": Region((0, 255, 0), (0, 1, 2), 415)}
 
 while True:
-    yellow.all_on()
-    time.sleep(0.5)
-    yellow.all_off()
-    blue.all_on()
-    time.sleep(0.5)
-    blue.all_off()
+    for color, region in regions.items():
+        region.all_on()
+        region.play_tone(0.5)
+        time.sleep(0.5)
+        region.all_off()
+        time.sleep(0.5)
